14ddef260f538901129f6684c99e5640008cc7b0
Grizzle <thejackshiva@gmail.com>
2021-09-10 13:27:08 +0545

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName('play')
     .setDescription('Starts playing Lo-fi radio station!'),
   async execute(interaction, client) {
-    client.func.play(interaction, process.env.LOFI_STREAM, 'Lo-fi Beats')
+    client.func.play(interaction, Date.now() % 2 === 0 ? process.env.LOFI_1 : process.env.LOFI_2, 'Lo-fi Beats')
   },
 };
