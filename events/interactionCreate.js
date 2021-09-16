c9b479128c537915e49776b55370fde39170829f
Grizzle <thejackshiva@gmail.com>
2021-09-16 21:03:51 +0545

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,11 @@
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client, player) {
+    if (interaction.isButton()) {
+      if (interaction.customId === 'play') {
+        client.func.play(interaction, Date.now() % 2 === 0 ? process.env.LOFI_1 : process.env.LOFI_2, 'Lo-fi Beats')
+      } else return;
+    }
     if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
